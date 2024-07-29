49a842ecb6e9f4cb6ffd14b2df879dd2348f7740
Вадим Давыдченко <vdavidchenko@mobiledep.ru>
2024-07-29 13:01:48 +0300

    add function readInput and convertCurrency

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import "fmt"
 
+func readInputCurrency() float64 {
+	var amount float64
+	fmt.Print("Enter the amount of the original currency:")
+	fmt.Scan(&amount)
+	return amount
+}
+
+func convertCurrency(amount float64, sourceCurrency string, targetCurrency string) float64 {
+
+}
+
 func main() {
 	const USDToEUR = 0.92
 	const USDToRUB = 85.41
+
+	// userInput := readInputCurrency()
 	EURToRUB := USDToRUB / USDToEUR
 	fmt.Println(EURToRUB)
 }
