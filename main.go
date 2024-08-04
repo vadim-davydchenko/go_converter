c1c2977af9b035be49597fe34a6b9fa7fdc4a870
Vadim <intro4life@mail.ru>
2024-08-04 12:25:47 +0300

    add check input currency and amount

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const USDToEUR = 0.92
-const USDToRUB = 85.41
+const (
+	USDToEUR = 0.92
+	USDToRUB = 85.41
+	EURToUSD = 1 / USDToEUR
+	EURToRUB = USDToRUB / USDToEUR
+	RUBToUSD = 1 / USDToRUB
+	RUBToEUR = 1 / EURToRUB
+)
 
 func main() {
 	fmt.Println("__Converter currency__")
@@ -11,35 +19,49 @@ func main() {
 	fmt.Println(sourceCurrency)
 	fmt.Println(amount)
 	fmt.Println(targetCurrency)
-	// EURToRUB := USDToRUB / USDToEUR
-	// fmt.Println(EURToRUB)
-
 }
 
 func getInput() (string, float64, string) {
 	var sourceCurrency string
 	var amount float64
 	var targetCurrency string
-	fmt.Print("Entering the source currency (USD,EUR,RUB):")
-	fmt.Scan(&sourceCurrency)
-	fmt.Print("Entering the amount:")
-	fmt.Scan(&amount)
-	switch sourceCurrency {
-	case "USD":
-		fmt.Print("Entering the target currency (EUR,RUB):")
-	case "EUR":
-		fmt.Print("Entering the target currency (USD,RUB):")
-	case "RUB":
-		fmt.Print("Entering the target currency (USD,EUR):")
+	for {
+		fmt.Print("Entering the source currency (USD,EUR,RUB):")
+		fmt.Scan(&sourceCurrency)
+		if sourceCurrency == "USD" || sourceCurrency == "EUR" || sourceCurrency == "RUB" {
+			break
+		}
+		fmt.Println("Invalid source currency. Please enter USD, EUR,RUB")
 	}
-	fmt.Scan(&targetCurrency)
+	for {
+		fmt.Print("Entering the amount:")
+		_, err := fmt.Scan(&amount)
+		if amount > 0 && err == nil {
+			break
+		}
+		fmt.Println("Invalid amount.Please enter a number and the number must be greater than zero")
+		fmt.Scanln()
+	}
+	for {
+		switch sourceCurrency {
+		case "USD":
+			fmt.Print("Entering the target currency (EUR,RUB):")
+		case "EUR":
+			fmt.Print("Entering the target currency (USD,RUB):")
+		case "RUB":
+			fmt.Print("Entering the target currency (USD,EUR):")
+		}
+		fmt.Scan(&targetCurrency)
+		if sourceCurrency == "USD" && (targetCurrency == "EUR" || targetCurrency == "RUB") ||
+			(sourceCurrency == "EUR" && (targetCurrency == "USD" || targetCurrency == "RUB")) ||
+			(sourceCurrency == "RUB" && (targetCurrency == "USD" || targetCurrency == "EUR")) {
+			break
+		}
+		fmt.Println("Invalid target currency. Please enter a valid currency according to the prompts")
+	}
 	return sourceCurrency, amount, targetCurrency
 }
 
-// func checkCurrencyAmount() bool {
-// 	var userChoise string
-// }
-
-// func convertCurrency(amount float64, sourceCurrency string, targetCurrency string) float64 {
-// 	return 0
-// }
+func convertCurrency(sourceCurrency string, amount float64, targetCurrency string) float64 {
+	return 0
+}
