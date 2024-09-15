18e93476d2829e35290fed24fcd175b45da41488
Vadim <intro4life@mail.ru>
2024-09-15 17:40:44 +0300

    add new structure for application JsonBin

---
+++ jsonBin/bins/bins.go
@@ -0,0 +1,37 @@
+package bins
+
+import "time"
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+type BinList struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func createBin(id, name string, private bool) *Bin {
+	newBin := &Bin{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+	return newBin
+}
+
+func createBinList(id, name string, private bool) *BinList {
+	newBinList := &BinList{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+	return newBinList
+}
